6506f3950240542431787e1d18fe39ec73d767f2
Nikolay Bryskin <nb@gosu.ai>
2020-05-08 15:57:30 +0300

    Django 3.0 compatibility

--- tinylinks/models.py
+++ tinylinks/models.py
@@ -1,12 +1,10 @@
 """Models for the ``django-tinylinks`` app."""
 from django.db import models
 from django.conf import settings
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.utils.timezone import now, timedelta
 
 
-@python_2_unicode_compatible
 class Tinylink(models.Model):
     """
     Model to 'translate' long URLs into small ones.
